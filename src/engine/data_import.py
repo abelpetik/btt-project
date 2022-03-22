15d1db2e4467ee16bb8aab9f33d0a34769c87714
MAnett13 <anettmatuscsak@gmail.com>
2022-03-22 19:53:49 +0100

    Read CSV. Reform the data. Save it.

--- src/engine/data_import.py
+++ src/engine/data_import.py
@@ -1,29 +1,15 @@
-
 import numpy as np
 import pandas as pd
 
-path_to_file = r"C:\Users\Anett\Documents\Msc\PPKE\3. semester\Brain Therapy Tech. (Dani)\Retina\Data from first visit\CB07_970322_220216105506_Small.csv"
-with open (path_to_file, "r") as f:
-    content = f.read()
-    elements = content.split(',')
-    myarray = np.array(elements)
-    # myarray_2d = myarray.reshape ((-1,36))
-    """
-    itt 2dimenziós tömbök akartam létrehozni az 1-ből de nem működött
-    """
-    print(myarray[:100])
 
-
-
-
-
-
-
-
-# encoding ='latin1'
-# Df = pd.read_csv(path_to_file, encoding="latin1", low_memory=False)
-# data = pd.read_table(path_to_file,skip_blank_lines=True, na_filter=True)
-# df=pd.read_csv(path_to_file).dropna()
-"""
-üres sorok kidobása
-"""
+path_to_file = r"C:\Users\Anett\Documents\Msc\PPKE\3. semester\Brain Therapy Tech\Retina\Data from first visit\CB07_970322_220216105506_Small.csv"
+df = pd.read_csv (path_to_file, sep= ";", encoding = 'unicode_escape',  header= None, low_memory=False)
+data = df.isna().sum() # Detect missing values. Count the NaN values in columns
+empty = data.values.tolist()
+max_value = max(empty)
+limit = max_value-20
+ind = (data[data >= limit].index)
+drop_cols = df.drop ( columns = ind )
+drop_cols.to_csv(r"C:\Users\Anett\Documents\Msc\PPKE\3. semester\Brain Therapy Tech\Retina\Data from first visit\CB07_970322_220216105506_Small_new.csv", \
+                 sep=",", encoding='Latin-1', index=False, header=False)
+print("Saved new data!")
